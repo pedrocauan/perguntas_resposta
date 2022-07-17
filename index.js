baa63b1e3fcabcab41c3a55db8ed2e4faa7b8889
pedrocauan <mainyasuo92@gmail.com>
2022-07-17 17:18:33 -0300

    Estruturação do projeto

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+const app = express()
+
+//Config
+    //View Engine
+    app.set("viewengine", "ejs") //acess o diretorio views
+    //Permite aplicação de arquivos css, javascript e imagem na pasta criada (public)
+    app.use(express.static("public"))
+
+app.get("/", function(req,res) {
+    res.render("index.ejs")
+})
+
+
+app.listen(8084, (err) => {
+    console.log("Rodou. Porta 8084")
+})
